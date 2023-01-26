58e1739f285b984cc2fc750b42459b0a01b9e6ef
Luevano-0709 <122376656+Luevano-0709@users.noreply.github.com>
2023-01-26 13:48:47 -0700

    Add files via upload
    Se mejoro el carrito

--- Carrito.js
+++ Carrito.js
@@ -1,15 +1,36 @@
 Vue.createApp({
     data(){
         return{
-            carrito: [],
-            producto: ''
+            nombre:'',
+            precio:'',
+            cantidad:'',
+            fecha:'',
+            sabor:'',
+            tipo:'',
+            productos: []
+/*             carrito: [],
+            producto: '' */
         }
     },
     methods: {
+        agregar(){
+            const producto = {
+                nombre:this.nombre,
+                precio:this.precio,
+                cantidad:this.cantidad,
+                fecha:this.fecha,
+                sabor:this.sabor,
+                tipo:this.tipo
+
+            }
+            this.productos.push(producto);
+        }
+    }
+    /* methods: {
         agregarProducto() {
         this.carrito.push(this.producto);   
         this.producto=''; 
         },
 
-    }
+    } */
 }).mount('#app')
