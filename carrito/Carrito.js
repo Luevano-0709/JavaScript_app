2aebdac6d88f6ca83b49b59005dff80b0e547535
Luevano-0709 <122376656+Luevano-0709@users.noreply.github.com>
2023-01-31 08:41:05 -0700

    actualizacion

---
+++ carrito/Carrito.js
@@ -0,0 +1,37 @@
+Vue.createApp({
+    data(){
+        return{
+            colorTable:'danger',
+            nombre:'',
+            precio:'',
+            cantidad:'',
+            fecha:'',
+            sabor:'',
+            tipo:'',
+            url:'',
+            productos: []
+/*             carrito: [],
+            producto: '' */
+        }
+    },
+    methods: {
+        agregar(){
+            const producto = {
+                nombre:this.nombre,
+                precio:this.precio,
+                cantidad:this.cantidad,
+                fecha:this.fecha,
+                sabor:this.sabor,
+                tipo:this.tipo,
+                url:this.url
+            }
+            this.productos.push(producto);
+        }
+    }
+    /* methods: {
+        agregarProducto() {
+        this.carrito.push(this.producto);   
+        this.producto=''; 
+        },
+    } */
+}).mount('#app')
